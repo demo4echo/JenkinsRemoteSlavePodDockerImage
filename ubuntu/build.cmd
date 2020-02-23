620acf0c24f21233287d10f7e98ea0c56e47d7c3
Tiran Meltser <tiran.meltser@efrat.com>
2020-02-23 15:08:56 +0200

    update

--- ubuntu/build.cmd
+++ ubuntu/build.cmd
@@ -4,8 +4,14 @@ SET REPOSITORY_NAME="demo4echo"
 SET IMAGE_NAME="ubuntu_k8scdk"
 SET IMAGE_TAG="latest"
 
+@REM Copy needed files from outside the build space
+COPY ..\.profile .
+
 @REM Build the image
 docker build -t %IMAGE_NAME%:%IMAGE_TAG% .
+
+@REM CDo some cleanup
+DEL .profile
 
 @REM Tag the image
 docker tag %IMAGE_NAME%:%IMAGE_TAG% %REPOSITORY_NAME%/%IMAGE_NAME%:%IMAGE_TAG%
